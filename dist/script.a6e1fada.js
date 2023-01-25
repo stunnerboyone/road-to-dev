cc976d2f1d24e5d1ff4c85e5073bd2c0e86be7ab
stunnerboyone <stunnerboy691@gmail.com>
2023-01-25 21:08:35 +0200

    footer

--- dist/script.a6e1fada.js
+++ dist/script.a6e1fada.js
@@ -144,7 +144,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55243" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58656" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
