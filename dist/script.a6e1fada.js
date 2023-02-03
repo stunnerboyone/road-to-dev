569a539acc0dd3c074b7bf15d058322059d74268
stunnerboyone <stunnerboy691@gmail.com>
2023-02-03 08:32:43 +0200

    count-parthers-home-screen-adaptive

--- dist/script.a6e1fada.js
+++ dist/script.a6e1fada.js
@@ -144,7 +144,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55964" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53579" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
